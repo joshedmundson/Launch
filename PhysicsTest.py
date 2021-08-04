e42952de6b5bf5d3a6d735ebb111673a728cc71e
Josh Edmundson <josh@edmundson.org>
2021-08-04 18:21:09 +0100

    Tested calcGravitationalPull method

--- PhysicsTest.py
+++ PhysicsTest.py
@@ -1,5 +1,12 @@
 import Physics as ph 
 import numpy as np 
 
-particle_1 = ph.Body(mass=1, position=np.array([0, 5]), velocity=np.array([0.1, 0]))
-particle_2 = ph.Body(mass=1, position=np.array([0, 0]), velocity=np.array([0, 0]))
+particle_1 = ph.Particle(mass=1, position=np.array([0, 5]), velocity=np.array([0.1, 0]))
+particle_2 = ph.Particle(mass=1, position=np.array([0, 0]), velocity=np.array([0, 0]))
+
+for particle in ph.Particle.particles:
+    particle.calcGravitationalPull()
+
+for particle in ph.Particle.particles:
+    print(particle.force)
+
