eafcb4ec4e717783e97379643858b13c8614c0a1
Josh Edmundson <josh@edmundson.org>
2021-08-04 17:25:51 +0100

    Added the Body and TimeStepper classes to Phys

---
+++ Physics.py
@@ -0,0 +1,20 @@
+import tkinter 
+import numpy as np 
+
+class Body:
+    '''A class to model the properties and behaviours of physical bodies'''
+
+    def __init__(self, mass=1, position=np.array([0, 0]), velocity=np.array([0, 0])):
+        self.mass = mass 
+        self.position = position 
+        self.velocity = velocity 
+        self.force = np.array([0, 0]) 
+
+
+class TimeStepper:
+    '''A class that updates the properties of Bodies using a Newtonian method'''
+
+    def __init__(self, delta_t=0.001):
+        self.delta_t = delta_t 
+    
+    
