0519356112a09c7165199ab36a17da9b126d064b
Josh Edmundson <josh@edmundson.org>
2021-08-13 20:25:53 +0100

    Test Fail: Particle.initOrbit() + TimeStepper()

--- PhysicsTest.py
+++ PhysicsTest.py
@@ -1,12 +1,26 @@
 import Physics as ph 
 import numpy as np 
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-particle_1 = ph.Particle(mass=1, position=np.array([0, 5]), velocity=np.array([0.1, 0]))
+sns.set()
+
+particle_1 = ph.Particle(mass=1, position=np.array([0, 5]), velocity=np.array([0, 0]))
 particle_2 = ph.Particle(mass=1, position=np.array([0, 0]), velocity=np.array([0, 0]))
+time_stepper = ph.TimeStepper()
 
 for particle in ph.Particle.particles:
     particle.calcGravitationalPull()
 
-for particle in ph.Particle.particles:
-    print(particle.force)
+particle_1.initOrbit(particle_2)
 
+x = []
+y = []
+
+for i in range(1, 10000):
+    time_stepper.step()
+    x.append(particle_2.position[0])
+    y.append(particle_2.position[1])
+
+plt.scatter(x, y)
+plt.show() 
